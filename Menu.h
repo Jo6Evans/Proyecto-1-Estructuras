e784fcb97cb067211dcacd3e62ec1fa11f1d2f33
JOSUE GUEVARA LOSTALO <josue.guevara.lostalo@est.una.ac.cr>
2024-09-10 19:17:20 -0600

    Creacion de clase menu

---
+++ Menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+#include <sstream>
+#include <iostream>
+using namespace std;
+	
+class Menu {
+private:
+
+public:
+	Menu();
+	void subMenu();
+
+
+};
+
+
+#endif // !MENU_H
